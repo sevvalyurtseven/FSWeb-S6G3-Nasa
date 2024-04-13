a2a748504cc6ed7996a58e9c7e6bd15845b8871f
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-13 04:39:03 +0300

    useEffect ile sayfa ilk render oldugunda veriyi cektik

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  axios
-    .get(
-      "https://api.nasa.gov/planetary/apod?api_key=0hwvX60oIkSCUNm1zvuTDZ84DbxIDgbDhqtTLEFz&date=2023-04-13"
-    )
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.err(error);
-    })
-    .finally(() => {
-      console.log("axios bitti");
-    });
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=0hwvX60oIkSCUNm1zvuTDZ84DbxIDgbDhqtTLEFz&date=2023-04-13"
+      )
+      .then((response) => {
+        //console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.err(error);
+      })
+      .finally(() => {
+        //console.log("axios bitti");
+      });
+  }, []);
+
   return (
     <div className="App">
       <p>
